b26a0bc328a48c2368a762e635db7fb2a62b9071
Yennie <lye0713@gmail.com>
2025-06-26 13:20:40 +0900

    feat: entry crud & member 테스트용

--- backend/src/main/java/com/diary/domain/member/entity/DiaryMember.java
+++ backend/src/main/java/com/diary/domain/member/entity/DiaryMember.java
@@ -1,5 +1,49 @@
 package com.diary.domain.member.entity;
 
+import java.time.LocalDateTime;
+
+import com.diary.domain.diary.entity.Diary;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "Diary_Member")
 public class DiaryMember {
-    
+    @EmbeddedId
+    private DiaryMemberId id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("diaryId")
+    @JoinColumn(name = "diary_id")
+    private Diary diary;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("userId")
+    @JoinColumn(name = "user_id")
+    private Member member;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role = Role.GUEST;
+
+    @Column(name = "invited_at")
+    private LocalDateTime invitedAt = LocalDateTime.now();
+
+    // enum
+    public enum Role {
+        OWNER, GUEST
+    }
 }
