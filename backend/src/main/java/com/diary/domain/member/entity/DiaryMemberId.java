ccab13152debf5ae562cc3e890327c2da84c6e38
Yennie <lye0713@gmail.com>
2025-07-01 14:06:29 +0900

    feat: 일기장 권한 확인
    - DiaryPermissionChecker 작성

--- backend/src/main/java/com/diary/domain/member/entity/DiaryMemberId.java
+++ backend/src/main/java/com/diary/domain/member/entity/DiaryMemberId.java
@@ -7,11 +7,13 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
-@NoArgsConstructor
+@Setter
 @EqualsAndHashCode
 @Embeddable
+@NoArgsConstructor
 @AllArgsConstructor
 public class DiaryMemberId implements Serializable {
 
