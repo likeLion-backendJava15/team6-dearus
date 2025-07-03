e20da4c0a362199aeab5b8fcf5f0b1c2e5c7a170
Yennie <lye0713@gmail.com>
2025-07-03 20:54:44 +0900

    refactor: DB 정보 비공개로 전환
    - backend 폴더에 .env 파일 생성 후 노션 프로젝트 세팅에 있는대로 입력

--- backend/src/main/java/com/diary/DearUsApplication.java
+++ backend/src/main/java/com/diary/DearUsApplication.java
@@ -2,10 +2,17 @@ package com.diary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class DearUsApplication {
+
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        
         SpringApplication.run(DearUsApplication.class, args);
     }
 
